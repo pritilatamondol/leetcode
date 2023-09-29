206904d1cc9e67ad26ad7af818da5606512579ed
pritilatamondol <71295075+pritilatamondol@users.noreply.github.com>
2023-09-29 11:26:39 -0700

    Update ReverseString.js

--- ReverseString.js
+++ ReverseString.js
@@ -22,3 +22,26 @@ function reverseString(str) {
       return reverseString(str.substr(1)) + str.charAt(0);
 }
 console.log(reverseString("hello i Am"));
+
+
+//To test a string is palindrome or not
+
+const palStr = "malayalam";
+function isPalindromeRegularWay(str) {
+	let len = str.length;
+  for(let i=0; i<len; i++){
+     if( str[i] != str[len-1-i]) {
+     		return false
+     }
+  }
+  return true;
+}
+console.log("isPalindrome:" + isPalindromeRegularWay(palStr));
+//Using in built function
+function isPalindrome(str) {
+    let revStr = str.split("").reverse().join("");
+    console.log(revStr);
+    return str == revStr ? true : false
+   
+ }
+ 
