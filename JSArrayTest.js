5738e616a2ddc1def567e447340439a9da8976ea
pritilatamondol <71295075+pritilatamondol@users.noreply.github.com>
2023-09-29 15:04:40 -0700

    Update JSArrayTest.js

--- JSArrayTest.js
+++ JSArrayTest.js
@@ -24,3 +24,23 @@ const total = [7, 2, 3, 4, 5].reduce( (previous, current) => {
  return previous+current}, 0);
       
 console.log("reduce method:",total); // 15
+//Another example of reduce
+const items = [
+   { name:"Auto", price:100},
+   {name:"Bikes", price:300},
+   {name:"Books", price:50},
+   {name:"Cell", price:20}
+]
+    console.log(items.filter( (item) =>  item.price<1000));
+    console.log(items.map(item =>item.price));
+    console.log(items.find(item =>item.price==50));
+    
+   /* items.forEach(item=> {
+       console.log(item.price);
+    })*/
+    
+    const total = items.reduce( (currentTotal, item)=>{
+    	return  item.price + currentTotal
+    }, 0)
+    
+console.log(total);
